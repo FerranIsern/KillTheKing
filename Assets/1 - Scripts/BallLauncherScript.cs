2dcf535944e78676a1fd8e95625e54a3dcf1c6b8
Ferran Isern Blasco <ferran@isern.blasco.cat>
2018-10-30 13:19:36 +0100

    first commit

---
+++ Assets/1 - Scripts/BallLauncherScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLauncherScript : MonoBehaviour {
+
+    public GameObject rock;
+    public float impulse;
+
+	public void ShootRock ()
+    {
+        var _rock = Instantiate(rock, transform.position, Quaternion.identity);
+        _rock.GetComponent<Rigidbody>().AddForce(gameObject.transform.forward * impulse, ForceMode.Impulse);
+    }
+}
---
+++ Assets/1 - Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public Animator catapultAnimator;
+    public BallLauncherScript ballLauncherScript;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began) || Input.GetKeyDown(KeyCode.Space))
+        {
+            catapultAnimator.SetTrigger("shoot");
+            Invoke("CallShot",0.15f);
+        }
+	}
+
+    void CallShot()
+    {
+        ballLauncherScript.ShootRock();
+    }
+}
